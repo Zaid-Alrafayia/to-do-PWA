92cf59f56f33ae03cf708917f154611b6d6d8f2c
noassociation5 <zaidomar678@gmail.com>
2024-12-14 20:44:22 +0300

    demo

---
+++ js/app.js
@@ -0,0 +1,56 @@
+const taskInput =document.getElementById("taskInput");
+const reminderTime = document.getElementById('reminderTime');
+const addTaskButton =document.getElementById("addTaskButton");
+const taskList =document.getElementById("taskList");
+
+//add task
+addTaskBtn.addEventListener('click', () => {
+    const taskText = taskInput.value.trim();
+    if (taskText) {
+      const li = document.createElement('li');
+      li.className = 'list-group-item d-flex justify-content-between align-items-center';
+      li.innerHTML = `
+        ${taskText}
+        <button class="btn btn-danger btn-sm delete-btn">Delete</button>
+      `;
+      taskList.appendChild(li);
+      taskInput.value = '';
+    }
+  });
+//remove task
+taskList.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete-btn')) {
+      e.target.parentElement.remove();
+    }
+  });
+  
+//save task to storage 
+window.addEventListener('unload', () => {
+    const tasks = Array.from(taskList.children).map(task => task.textContent.replace('Delete', '').trim());
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  });
+
+//load tasks to storage
+window.addEventListener('load', () => {
+    const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    tasks.forEach(taskText => {
+      const li = document.createElement('li');
+      li.className = 'list-group-item d-flex justify-content-between align-items-center';
+      li.innerHTML = `
+        ${taskText}
+        <button class="btn btn-danger btn-sm delete-btn">Delete</button>
+      `;
+      taskList.appendChild(li);
+    });
+  });
+
+
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
+  
