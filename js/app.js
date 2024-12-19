29168a45a1f21e4d197443acfc16dfbbd4708338
noassociation5 <zaidomar678@gmail.com>
2024-12-19 17:17:31 +0300

    checkbox

--- js/app.js
+++ js/app.js
@@ -1,52 +1,53 @@
 const taskInput =document.getElementById("taskInput");
 const addTaskButton =document.getElementById("addTaskButton");
 const taskList =document.getElementById("taskList");
-
-addTaskBtn.addEventListener('click', () => {
-    const taskText = taskInput.value.trim();
-    if (taskText) {
-      const li = document.createElement('li');
-      li.className = 'list-group-item d-flex justify-content-between align-items-center';
-      li.innerHTML = `
-      <input type="checkbox" disabled>
-        ${taskText}
-        <button class="btn btn-danger btn-sm delete-btn">Delete</button>
-      `;
-      taskList.appendChild(li);
-      taskInput.value = '';
-    }
-  });
-//remove task
-taskList.addEventListener('click', (e) => {
-    if (e.target.classList.contains('delete-btn')) {
-      e.target.parentElement.remove();
-    }
-  });
-  
-window.addEventListener('unload', () => {
-    const tasks = Array.from(taskList.children).map(task => task.textContent.replace('Delete', '').trim());
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  });
-
-window.addEventListener('load', () => {
-    const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
-    tasks.forEach(taskText => {
-      const li = document.createElement('li');
-      li.className = 'list-group-item d-flex justify-content-between align-items-center';
-      li.innerHTML = `
-        ${taskText}
-        <button class="btn btn-danger btn-sm delete-btn">Delete</button>
-      `;
-      taskList.appendChild(li);
+document.addEventListener('DOMContentLoaded', () => {
+  addTaskBtn.addEventListener('click', () => {
+      const taskText = taskInput.value.trim();
+      if (taskText) {
+        const li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+        li.innerHTML = `
+        <input class="" type="checkbox" disabled>
+          ${taskText}
+          <button class="btn btn-danger btn-sm delete-btn">Delete</button>
+        `;
+        taskList.appendChild(li);
+        taskInput.value = '';
+      }
     });
-  });
+  //remove task
+  taskList.addEventListener('click', (e) => {
+      if (e.target.classList.contains('delete-btn')) {
+        e.target.parentElement.remove();
+      }
+    });
+    
+  window.addEventListener('unload', () => {
+      const tasks = Array.from(taskList.children).map(task => task.textContent.replace('Delete', '').trim());
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    });
+
+  window.addEventListener('load', () => {
+      const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+      tasks.forEach(taskText => {
+        const li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+        li.innerHTML = `
+          ${taskText}
+          <button class="btn btn-danger btn-sm delete-btn">Delete</button>
+        `;
+        taskList.appendChild(li);
+      });
+    });
 
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/to-do-PWA/html/service-worker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
-}
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/to-do-PWA/html/service-worker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+      })
+    }
+});
